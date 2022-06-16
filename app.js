df2f7493dee562c3f509da3629e61865a8daded4
Yasin Yağmur <yasin.yagmur@gmail.com>
2022-06-16 14:18:54 +0300

    js file was created

---
+++ app.js
@@ -0,0 +1,11 @@
+const url;
+const key;
+
+
+const ınput = document.querySelector('#input');
+const search = document.querySelector('.search');
+const reset = document.querySelector('.reset')
+
+container.addEventListener('click',(e)=>{
+
+})
