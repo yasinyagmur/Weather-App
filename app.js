994569bfa840116dc2d256b1d9bd72cad9ff4477
Yasin Yağmur <yasin.yagmur@gmail.com>
2022-06-17 14:53:48 +0300

    fixed

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const input = document.querySelector("#input");
 const search = document.querySelector(".search");
 const card = document.querySelector(".card");
 const reset = document.querySelector(".reset");
+let description = document.querySelector(".description");
 
 const getWeatherInfo = async () => {
   if (card.innerHTML.toLowerCase().includes(input.value.toLowerCase())) {
-    alert(input.value + " is already exists");
+    description.innerText = `You already know the weather for city. Please search for another city 😉`;
+    setTimeout(() => {
+      description.innerText = "";
+    }, 5000);
+    input.value = "";
   } else {
     const key = "c101019b3917ef4c27af74063e4ec864";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=${key}`;
@@ -14,12 +19,25 @@ const getWeatherInfo = async () => {
       const weatherInfo = await response.json();
 
       // console.log(weatherInfo);
-      const { weather, main, name } = weatherInfo;
+      const { weather, main, name, sys } = weatherInfo;
       // console.log(weather,main,name);
-      card.innerHTML += `${main.temp}<br> ${name}<br> ${weather[0].description}<br> <img src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png"/> <br> `;
+      card.innerHTML += `<div class='cardInfo'>
+      <h2 class="city-name">
+              <span>${name}</span>
+              <sup>${sys.country}</sup>
+          </h2>
+          <div class="city-temp">${main.temp}<sup>°C</sup></div>
+          <figure>
+              <img class="city-icon" src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png">
+              <figcaption>${weather[0].description}</figcaption>
+          </figure>
+          </div>`;
       input.value = "";
     } catch (error) {
-      alert("there is a not city called " + input.value);
+      description.innerText = error;
+      setTimeout(() => {
+        description.innerText = "";
+      }, 5000);
     } finally {
       input.value = "";
     }
@@ -30,3 +48,9 @@ const resetWindow = () => {
 };
 search.addEventListener("click", getWeatherInfo);
 reset.addEventListener("click", resetWindow);
+
+input.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    getWeatherInfo();
+  }
+});
