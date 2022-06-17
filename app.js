00de05e4bcb42cc808af4e6be11deec90148da93
Yasin Yağmur <yasin.yagmur@gmail.com>
2022-06-17 13:23:49 +0300

    fixed

--- app.js
+++ app.js
@@ -1,37 +1,32 @@
+const input = document.querySelector("#input");
+const search = document.querySelector(".search");
+const card = document.querySelector(".card");
+const reset = document.querySelector(".reset");
 
-const input = document.querySelector('#input');
-const search = document.querySelector('.search');
-const card = document.querySelector('.card')
-const reset = document.querySelector('.reset')
-
-const getWeatherInfo= async ()=>{
-   if(card.innerHTML.toLowerCase().includes(input.value.toLowerCase())){
-    alert(input.value+" is already exists")
-   }else{const key = 'c101019b3917ef4c27af74063e4ec864';
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=${key}`
+const getWeatherInfo = async () => {
+  if (card.innerHTML.toLowerCase().includes(input.value.toLowerCase())) {
+    alert(input.value + " is already exists");
+  } else {
+    const key = "c101019b3917ef4c27af74063e4ec864";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=${key}`;
     try {
-        const response = await fetch(url);
-        const weatherInfo = await response.json();
+      const response = await fetch(url);
+      const weatherInfo = await response.json();
 
-        console.log(weatherInfo);
-        const{weather,main,name,}=weatherInfo
-        console.log(weather,main,name);
-        card.innerHTML +=`${main.temp}<br> ${name}<br> ${weather[0].description}<br> <img src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png"/> <br> `
-        input.value = ''
-
-
-
+      // console.log(weatherInfo);
+      const { weather, main, name } = weatherInfo;
+      // console.log(weather,main,name);
+      card.innerHTML += `${main.temp}<br> ${name}<br> ${weather[0].description}<br> <img src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png"/> <br> `;
+      input.value = "";
     } catch (error) {
-    alert('there is a not city called ' + input.value)
-    }finally{
-        input.value =''
+      alert("there is a not city called " + input.value);
+    } finally {
+      input.value = "";
     }
-}
-    
-
-}
-const resetWindow =()=>{
-    location.reload();
-}
-search.addEventListener('click',getWeatherInfo);
-reset.addEventListener('click',resetWindow);
+  }
+};
+const resetWindow = () => {
+  location.reload();
+};
+search.addEventListener("click", getWeatherInfo);
+reset.addEventListener("click", resetWindow);
